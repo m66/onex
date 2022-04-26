214678919629307f7ac514b5e3e32b98c9ea9bbf
Karen Manasyan <kar.manasyan98@gmail.com>
2022-04-26 06:32:32 -0700

    add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const contentInfo = [
+    {
+      logoUrl: 'photo-camera.svg',
+      text: 'Լուսանկարել դրսից'
+    },
+    {
+      logoUrl: 'photos-inside.svg',
+      text: 'Լուսանկարել ներսից'
+    },
+    {
+      logoUrl: 'stop.svg',
+      text: 'Կանգնեցնել'
+    },
+    {
+      logoUrl: 'check-order.svg',
+      text: 'Ստուգել պատվերը'
+    },
+    {
+      logoUrl: 'single-package.svg',
+      text: 'Չվերափաթեթավորել'
+    },
+    {
+      logoUrl: 'repackaging.svg',
+      text: 'Հավելյալ վերափաթեթավորում'
+    },
+    {
+      logoUrl: 'change-receiver.svg',
+      text: 'Փոխել ստացողին'
+    },
+    {
+      logoUrl: 'unboxing.svg',
+      text: 'Բաժանել մասերի'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1><span>SMART </span>ԾԱՌԱՅՈՒԹՅՈՒՆՆԵՐ</h1>
+        <p>Նախքան առաքանու ԱՄՆ կամ Չինաստանի պահեստ հասնելը դուք կարող եք պատվիրել հետևյալ SMART ծառայությունները</p>
+      </div>
+      <div className='content'>
+        <div className='content-img'>
+          <img src='https://onex.am/images/smart-service.svg' />
+        </div>
+        <div className='info'>
+          <ul>
+            {
+              contentInfo.map(item => (
+                <li key={ Math.random() }><img src={ `https://onex.am/images/smart_services/${item.logoUrl}` } />{ item.text }</li>
+              ))
+            }
+          </ul>
+          <button>Ավելին</button>
+        </div>
+      </div>
     </div>
   );
 }
